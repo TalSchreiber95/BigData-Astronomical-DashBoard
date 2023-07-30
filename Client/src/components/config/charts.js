2a049a8799dde801ae05c89f8b126b1306ca4f55
talschreiber95 <talfreestyle@gmail.com>
2023-07-30 21:16:18 +0300

    semi changes

--- Client/src/components/config/charts.js
+++ Client/src/components/config/charts.js
@@ -1,7 +1,7 @@
 export const ChartDetailsConfig = [
   { name: "Asteroids close to Earth (monthly)", type: "pie" },
   { name: "Events urgency", type: "pie" },
+  { name: "Events Distribution", type: "bar" },
   { name: "Sun's Activities (X-ray level)", type: "line" },
   { name: "Sun's Weather (Hourly)", type: "line" },
-  { name: "Events Distribution", type: "bar" },
 ];
